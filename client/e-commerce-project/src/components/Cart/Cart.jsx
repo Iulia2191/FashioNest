754aa3ba6add3e892d805fc9d409404c0ea46888
Iulia2191 <125976840+Iulia2191@users.noreply.github.com>
2023-10-01 18:54:09 +0200

    cart reducer

--- client/e-commerce-project/src/components/Cart/Cart.jsx
+++ client/e-commerce-project/src/components/Cart/Cart.jsx
@@ -1,32 +1,14 @@
 import DeleteIcon from '@mui/icons-material/Delete';
 import './Cart.scss'
+import { useSelector } from 'react-redux/es/hooks/useSelector';
 function Cart () {
-  const data = [
-    {
-      id: 1,
-      img: '../img/full-length-portrait-cheerful-ginger-woman-romantic-caucasian-girl-orange-clothes.jpg',
-      title: 'Orange Skirt',
-      desc: 'Orange Skirt',
-      isNew: false,
-      oldPrice: 78,
-      price: 50
-    },
-    {
-      id: 2,
-      img: '../img/wonderful-young-woman-with-happy-smile-posing-bright-purple-background.jpg',
-      title: 'Purple fur coat',
-      desc: 'Purple fur coat',
-      isNew: true,
-      oldPrice: 95,
-      price: 70
-    }
-  ]
+  const products = useSelector(state=>state.cart.products)
   return (
     <div className='cart'>
       <h2>Products in your cart</h2>
-      {data?.map(item => (
+      {products?.map(item => (
         <div className='item' key={item.id}>
-          <img src={item.img} alt='' />
+          <img src={import.meta.env.VITE_APP_UPLOAD_URL + item.img} alt='' />
           <div className='details'>
             <h3>{item.title}</h3>
             <p>{item.desc.substring(0,100)}</p>
--- client/e-commerce-project/src/pages/Product/Product.jsx
+++ client/e-commerce-project/src/pages/Product/Product.jsx
@@ -5,12 +5,14 @@ import BalanceIcon from '@mui/icons-material/Balance';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import useFetch from '../../hooks/useFetch';
 import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../redux/cartReducer';
 
 const Product = () => {
   const id = useParams().id;
   const [selectImg, setSelectImg] = React.useState("img");
   const [quantity, setQuantity] = React.useState(1);
-
+const dispatch = useDispatch()
   const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
 
   if (loading) {
@@ -45,7 +47,14 @@ const Product = () => {
           {quantity}
           <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
         </div>
-        <button className="add">
+        <button className="add" onClick={()=> dispatch(addToCart({
+          id: data.attributes.id,
+          title: data.attributes.title,
+          desc: data.attributes.desc,
+          price: data.attributes.price,
+          img: data.attributes.img.attributes.url,
+          quantity
+        }))}>
           <AddShoppingCartIcon />ADD TO CART
         </button>
         <div className="link">
