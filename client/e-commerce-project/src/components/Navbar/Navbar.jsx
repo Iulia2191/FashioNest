dc93029f3eb692a230c05154b6d22e2ce47d590f
Iulia2191 <125976840+Iulia2191@users.noreply.github.com>
2023-10-09 17:49:04 +0200

    favorite message

--- client/e-commerce-project/src/components/Navbar/Navbar.jsx
+++ client/e-commerce-project/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import SearchIcon from '@mui/icons-material/Search'
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
@@ -10,20 +10,34 @@ import './Navbar.scss'
 import { useSelector } from 'react-redux/es/hooks/useSelector'
 
 const Navbar = () => {
-
   const [openCart, setOpenCart] = useState(false)
   const [menuOpen, setMenuOpen] = useState(false)
-  const products = useSelector((state) => state.cart.products)
+  const products = useSelector(state => state.cart.products)
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen)
   }
+  useEffect(() => {
+    if (menuOpen) {
+      const closeMenuOnOutsideClick = (e) => {
+        if (!e.target.closest('.navbar')) {
+          
+          setMenuOpen(false);
+        }
+      };
 
+      document.addEventListener('click', closeMenuOnOutsideClick);
+      return () => {
+        document.removeEventListener('click', closeMenuOnOutsideClick);
+      };
+    }
+  }, [menuOpen]);
   return (
     <div className={`navbar ${menuOpen ? 'menu-open' : ''}`}>
       <div className='menu-icon' onClick={toggleMenu}>
         <MenuIcon />
       </div>
+
       <div className='center'>
         <Link className='link' to='/'>
           FashioNest
@@ -38,11 +52,11 @@ const Navbar = () => {
             <PersonOutlineIcon />
           </div>
           <Link className='link' to='/favorite'>
-          <div className='icon'>
-            <FavoriteBorderIcon />
-          </div>
+            <div className='icon'>
+              <FavoriteBorderIcon />
+            </div>
           </Link>
-          <div className='cartIcon' onClick={()=> setOpenCart(!openCart)}>
+          <div className='cartIcon' onClick={() => setOpenCart(!openCart)}>
             <ShoppingCartIcon />
             <span>{products.length}</span>
           </div>
@@ -94,7 +108,13 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-      {openCart && <Cart/>}
+       {menuOpen && (
+        <div
+          className='background'
+          onClick={toggleMenu}
+        ></div>
+      )}
+      {openCart && <Cart />}
     </div>
   )
 }
--- client/e-commerce-project/src/pages/Favorite/Favorite.jsx
+++ client/e-commerce-project/src/pages/Favorite/Favorite.jsx
@@ -1,72 +1,67 @@
-import React from 'react'
-import './Favorite.scss'
-import { useSelector, useDispatch } from 'react-redux'
-import { removeItemFavorite, resetFavorite } from '../../redux/favoriteReducer'
-import { useParams } from 'react-router-dom'
-import { addToCart, removeItem } from '../../redux/cartReducer'
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
-import useFetch from '../../hooks/useFetch'
+import React from 'react';
+import './Favorite.scss';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeItemFavorite, resetFavorite } from '../../redux/favoriteReducer';
+import { useParams } from 'react-router-dom';
+import { addToCart, removeItem } from '../../redux/cartReducer';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import useFetch from '../../hooks/useFetch';
 
-function Favorite () {
-    const id = useParams().id;
-    const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
-    
-  const products = useSelector(state => state.favorite.products) || []
-  const dispatch = useDispatch()
+function Favorite() {
+  const id = useParams().id;
+  const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
+
+  const products = useSelector((state) => state.favorite.products) || [];
+  const dispatch = useDispatch();
 
   return (
     <div className='favorite'>
       <h2>Favorite Products</h2>
       <div className='wrapper'>
-        {products.map(item => (
-          <div className='item' key={item.id}>
-            <img src={import.meta.env.VITE_APP_UPLOAD_URL + item.img} alt='' />
-            <div className='details'>
-              <h3>{item.title}</h3>
-              <div className='price'>$ {item.price}</div>
-            </div>
-            <button
-  className='add'
-  onClick={() =>
-    dispatch(
-      addToCart({
-        id: item.id, 
-        title: item.title,
-        desc: item.desc,
-        price: item.price,
-        img: item.img,
-        quantity: 1,
-      })
-    )
-  }
->
-  <AddShoppingCartIcon /> ADD TO CART
-</button>
+        {products.length === 0 ? ( 
+            
+          <p>You do not have favorite products.</p>
+        ) : (
+          products.map((item) => (
+            <div className='item' key={item.id}>
+              <img src={import.meta.env.VITE_APP_UPLOAD_URL + item.img} alt='' />
+              <div className='details'>
+                <h3>{item.title}</h3>
+                <div className='price'>$ {item.price}</div>
+              </div>
+              <button
+                className='add'
+                onClick={() =>
+                  dispatch(
+                    addToCart({
+                      id: item.id,
+                      title: item.title,
+                      desc: item.desc,
+                      price: item.price,
+                      img: item.img,
+                      quantity: 1,
+                    })
+                  )
+                }
+              >
+                <AddShoppingCartIcon /> ADD TO CART
+              </button>
 
-            <button
-              className='delete'
-              onClick={() => dispatch(removeItemFavorite(item.id))} 
-            >
-              Remove
-            </button>
-          </div>
-        ))}
-        
+              <button
+                className='delete'
+                onClick={() => dispatch(removeItemFavorite(item.id))}
+              >
+                Remove
+              </button>
+            </div>
+          ))
+        )}
       </div>
       <span className='reset' onClick={() => dispatch(resetFavorite())}>
         Reset Favorites
       </span>
     </div>
-  )
+  );
 }
 
 export default Favorite;
-
-
-
-
-
-
-
-
-
